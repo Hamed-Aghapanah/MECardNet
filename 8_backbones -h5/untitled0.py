f872ee1ca47cc134b59755b30c0a8c00006039a9
EidRadiolas <alisaeeidirad@proton.me>
2024-11-24 09:55:53 -0500

    DELETE OLD FILES

--- 8_backbones -h5/untitled0.py
+++
@@ -1,6 +0,0 @@
-plt.annotate(f'min Time ( Second) : {min(times)} ({min_times_backbone})', 
-             xy=(min_times_index, min(times)), 
-             xytext=( -60, -60), 
-             textcoords='offset points', 
-             arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2"),
-             fontname='Times New Roman')  # Set font to Times New Roman
